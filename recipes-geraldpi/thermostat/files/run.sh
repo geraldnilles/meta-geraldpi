95d2fecd978af5582851c4ba687bcf8f182aed95
Gerald Nilles <gerald@nilleye.com>
2022-12-04 10:27:05 -0800

    Add TOdos

--- recipes-geraldpi/thermostat/files/run.sh
+++ recipes-geraldpi/thermostat/files/run.sh
@@ -3,6 +3,10 @@
 # Thermostat Run Script. 
 # This script will check the temperature and turn on the heat as needed.
 # This shoudl be called periodically by a systemd routing
+#
+# TODO Conver this into python so its eaiser to compare floats.  RIght now, we
+# are converting everything to whole number which forces us to have a lot of
+# hysteresis
 
 cd "$(dirname "$0")"
 
@@ -23,4 +27,6 @@ then
 	echo "Turning Heat Off"
 	./heat.sh 0
 fi
+
+# TODO Add range for enabling cooling
 
