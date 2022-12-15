4e034481781cf8aae0d86a057005e0e28b25ee70
Gerald Nilles <gerald@nilleye.com>
2022-12-14 19:45:00 -0800

    improve history

--- recipes-geraldpi/thermostat/files/history.py
+++ recipes-geraldpi/thermostat/files/history.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
+#
+####################
+# History
+####################
+#
+# THis script prints the the most recent 2 horus of temperature data.  It uses
+# resample to downsample to 5 minute increments. 
+#
+####################
 
 import asyncio
+import pandas as pd
 
 import scan
 
 async def main():
     db = await scan.client()
     df = db[db["Temp"] > 50]
-    #ax = df[df["Room"] == "Bedroom Temperature"].plot(x="Time",y="Temp", ylabel="Bedroom")
-    #df[df["Room"] == "Master Temperature"].plot(x="Time",y="Temp", ylabel="Master",ax=ax)
-    #df.plot(x="Time",y="Temp")
-    #plt.show()
-    print(df.tail(50))
+    #print(df.tail(50))
+    for room in pd.unique(db["Room"]):
+        print(room)
+        d = df[df["Room"] == room]
+        print(d.resample("5min",on="Time").mean().tail(25))
 
 asyncio.run(main())
 
