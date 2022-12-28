d0a165c72782559fbae663862402cd57224a4460
Gerald Nilles <gerald@nilleye.com>
2022-12-27 19:11:06 -0800

    Enable Fan when heat is on

--- recipes-geraldpi/thermostat/files/heat.sh
+++ recipes-geraldpi/thermostat/files/heat.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 
 echo "Entering Heat Mode"
 
+./gpio.sh 20 1 # Not Required, but i think this makes more sense from a state trasition POV
 ./gpio.sh 26 1
 ./gpio.sh 21 0
-./gpio.sh 20 0
 
