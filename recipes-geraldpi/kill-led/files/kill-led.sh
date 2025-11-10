f83236a47e9e4989e2deb538aacdbf597ff64a56
Gerald Nilles <geraldnilles@gmail.com>
2025-11-10 13:45:10 -0800

    Update Kill LED script to work with newer kernel

--- recipes-geraldpi/kill-led/files/kill-led.sh
+++ recipes-geraldpi/kill-led/files/kill-led.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-echo 0 > /sys/class/leds/led0/brightness
+echo 0 > /sys/class/leds/ACT/brightness
+echo 0 > /sys/class/leds/default-on/brightness
+#echo 0 > /sys/class/leds/led0/brightness
 
